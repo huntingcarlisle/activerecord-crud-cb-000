f600aea246a961c6c6f11c490b53b44e43fce3aa
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 22:41:45 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -87,7 +87,7 @@ def can_update_multiple_items_at_once
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
   Movie.all.each do |m|
-   
+   m.update(title: "A Movie")
   end 
 end
 
